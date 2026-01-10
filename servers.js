5c8ec06fd058bbb8b557fba6526ad6a52e983cc5
Lito Poloyapoy <159044455+litspoloyaps@users.noreply.github.com>
2026-01-10 13:14:35 +0800

    Update servers.js

--- servers.js
+++ servers.js
@@ -1,5 +1,38 @@
-const PORT = process.env.PORT || 3000;
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+let users = 0;
+const MAX_USERS = 2;
+
+io.on("connection", (socket) => {
+
+    if (users >= MAX_USERS) {
+        socket.emit("room-full");
+        socket.disconnect(true);
+        return;
+    }
+
+    users++;
+    console.log("User connected:", users);
+
+    socket.on("chat message", (msg) => {
+        socket.broadcast.emit("chat message", msg);
+    });
+
+    socket.on("disconnect", () => {
+        users--;
+        console.log("User disconnected:", users);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log("Server running on port " + PORT);
 });
